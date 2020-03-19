025c9a1db767db59ed892470d18aa92537464ea7
Antonio Piazza <antman1p@gmail.com>
2020-03-19 16:44:02 -0500

    open tab info is outputting to log temporarily

--- red_refresh.js
+++ red_refresh.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", listTabs);
 
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true});
+}
+
+function listTabs() {
+  getCurrentWindowTabs().then((tabs) => {
+    for (let tab of tabs) {
+      console.log(tab.title);
+    }
+  });
+
+}
