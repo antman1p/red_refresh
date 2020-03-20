88cf9c77aca0d6374b3f1dddfebfe28b68936d11
Antonio Piazza <antman1p@gmail.com>
2020-03-20 17:20:01 -0500

    fixed row select issue

--- js/table-select.js
+++
@@ -1,5 +0,0 @@
-$('#activeTimersTable tbody tr').click(function(){
-  console.log("I'm above!");
-  $(this).addClass('table-info').siblings().removeClass('table-info');
-  console.log("I'm below!");
-});
--- red_refresh.js
+++ red_refresh.js
@@ -1,25 +1,34 @@
-// document.addEventListener("DOMContentLoaded", listTabs);
-//
-// function getCurrentWindowTabs() {
-//   return browser.tabs.query({currentWindow: true});
-// }
+document.addEventListener("DOMContentLoaded", listTabs);
 
-// function listTabs() {
-//   getCurrentWindowTabs().then((tabs) => {
-//     var tableBod = document.getElementById("activeTimersTable").getElementsByTagName('tbody')[0];
-//     for (let tab of tabs) {
-//       var row = tableBod.insertRow();
-//       row.className = 'table-default';
-//       row.scope = 'row';
-//       var cell1 = row.insertCell(0);
-//       var cell2 = row.insertCell(1);
-//       var cell3 = row.insertCell(2);
-//       var cell4 = row.insertCell(3);
-//
-//       cell1.innerHTML = "Inactive";
-//       cell2.innerHTML = tab.title;
-//       cell3.innerHTML = " - ";
-//       cell4.innerHTML = " - ";
-//     }
-//   });
-// }
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true});
+}
+
+function listTabs() {
+  getCurrentWindowTabs().then((tabs) => {
+    var tableBod = document.getElementById("activeTimersTable").getElementsByTagName('tbody')[0];
+    for (let tab of tabs) {
+      var row = tableBod.insertRow();
+      row.className = 'table-default';
+      row.scope = 'row';
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+
+      cell1.innerHTML = "Inactive";
+      cell2.innerHTML = tab.title;
+      cell3.innerHTML = " - ";
+      cell4.innerHTML = " - ";
+    }
+    tableSelect()
+  });
+}
+
+function tableSelect() {
+  $('#activeTimersTable tbody tr').click(function(){
+    console.log("I'm above!");
+    $(this).addClass('table-info').siblings().removeClass('table-info');
+    console.log("I'm below!");
+  });
+}
