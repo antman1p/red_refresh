a1a300b874821025ce0aa59e7b714e2bebabe621
Antonio Piazza <antman1p@gmail.com>
2020-03-20 14:48:54 -0500

    added bootstrap.map.js file.  Still debugging row select issue

--- js/table-select.js
+++ js/table-select.js
@@ -1,4 +1,5 @@
-
 $('#activeTimersTable tbody tr').click(function(){
+  console.log("I'm above!");
   $(this).addClass('table-info').siblings().removeClass('table-info');
+  console.log("I'm below!");
 });
