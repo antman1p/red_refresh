010cbdd4807ef3218e773008e04ebc06b3274970
Antonio Piazza <antman1p@gmail.com>
2020-03-24 13:40:41 -0500

    Button calls funciton.  Working on function to start refresh timer

--- red_refresh.js
+++ red_refresh.js
@@ -5,21 +5,23 @@ function getCurrentWindowTabs() {
 }
 
 function listTabs() {
-  getCurrentWindowTabs().then((tabs) => {
+  var currentTabs = getCurrentWindowTabs().then((tabs) => {
     var tableBod = document.getElementById("activeTimersTable").getElementsByTagName('tbody')[0];
     for (let tab of tabs) {
       var row = tableBod.insertRow();
       row.className = 'table-default';
       row.scope = 'row';
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
+      var cell0 = row.insertCell(0);
+      var cell1 = row.insertCell(1);
+      var cell2 = row.insertCell(2);
+      var cell3 = row.insertCell(3);
+      var cell4 = row.insertCell(4);
 
-      cell1.innerHTML = "Inactive";
-      cell2.innerHTML = tab.title;
+      cell0.innerHTML = "Inactive";
+      cell1.innerHTML = tab.title;
+      cell2.innerHTML = " - ";
       cell3.innerHTML = " - ";
-      cell4.innerHTML = " - ";
+      cell4.innerHTML = tab.id;
     }
     tableSelect();
   });
@@ -27,6 +29,7 @@ function listTabs() {
 
 function tableSelect() {
   $('#activeTimersTable tbody tr').click(function(){
+    $(this).removeClass("table-default").siblings().addClass("table-default");
     $(this).addClass('table-info').siblings().removeClass('table-info');
     enableActivateButton();
     enableIntevalTB();
@@ -40,4 +43,20 @@ function enableActivateButton() {
 
 function enableIntevalTB() {
   document.getElementById("inputInterval").disabled = false;
+}
+
+function activateRefreshTimer() {
+  var rows = document.getElementById("activeTimersTable").getElementsByTagName('tr');
+  console.log(rows);
+  for (row in rows){
+
+  }
+}
+
+  $('#actBtn').click(function(){
+    var actvTmrsTbl = document.getElementById("activeTimersTable");
+    activateRefreshTimer();
+  });
+
+function startNewTimer() {
 }
